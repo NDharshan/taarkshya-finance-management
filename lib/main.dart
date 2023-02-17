da90495aa44ee5d8084b9cfa884804bd5c5eee18
NDharshan <98468801+NDharshan@users.noreply.github.com>
2023-02-18 00:11:50 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -24,102 +24,248 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Stack(
-        children: [
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 240,
-                decoration: const BoxDecoration(
-                    color: Color(0xff368983),
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                    )),
-                child: Stack(
-                  children: [
-                    Positioned(
-                        top: 5,
-                        left: 340,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(7),
-                          child: Container(
-                            height: 40,
-                            width: 40,
-                            color: const Color.fromRGBO(250, 250, 250, 0.1),
-                            child: const Icon(
-                              Icons.notification_add_outlined,
-                              size: 30,
-                              color: Colors.white,
-                            ),
-                          ),
-                        )),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 35, left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'Good afternoon',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16,
-                                color: Color.fromARGB(255, 255, 255, 223)),
-                          ),
-                          Text(
-                            'Kamala Srinivas',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 16,
-                                color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
+          child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: SizedBox(height: 340, child: _head()),
           ),
-          Positioned(
-            top: 160,
-            left: 37,
-            child: Container(
-              height: 170,
-              width: 320,
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 47, 125, 121),
-                  borderRadius: BorderRadius.circular(15)),
-              child: Column(
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Transactions History',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 19,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 19,
+                          color: Colors.grey),
+                    ),
+                  ]),
+            ),
+          ),
+          SliverList(delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.asset(
+                    'images/Transfer.png',
+                    height: 40,
+                  ),
+                ),
+              );
+            },
+          ))
+        ],
+      )),
+    );
+  }
+
+  Widget _head() {
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: const BoxDecoration(
+                  color: Color(0xff368983),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  )),
+              child: Stack(
                 children: [
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(
-                            'Total Balance',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16,
-                                color: Colors.white),
-                          ),
-                          Icon(
-                            Icons.more_horiz,
+                  Positioned(
+                      top: 5,
+                      left: 340,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(7),
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          color: const Color.fromRGBO(250, 250, 250, 0.1),
+                          child: const Icon(
+                            Icons.notification_add_outlined,
+                            size: 30,
                             color: Colors.white,
-                          )
-                        ]),
-                  ),
+                          ),
+                        ),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 35, left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Good afternoon',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 255, 255, 223)),
+                        ),
+                        Text(
+                          'Kamala Srinivas',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
-          )
-        ],
-      )),
+          ],
+        ),
+        Positioned(
+          top: 160,
+          left: 37,
+          child: Container(
+            height: 170,
+            width: 320,
+            decoration: BoxDecoration(
+                boxShadow: const [
+                  BoxShadow(
+                      color: Color.fromARGB(47, 125, 121, 103),
+                      offset: Offset(0, 6),
+                      blurRadius: 12,
+                      spreadRadius: 6)
+                ],
+                color: const Color.fromARGB(255, 47, 125, 121),
+                borderRadius: BorderRadius.circular(15)),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text(
+                          'Total Balance',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Colors.white),
+                        ),
+                        Icon(
+                          Icons.more_horiz,
+                          color: Colors.white,
+                        )
+                      ]),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Row(
+                    children: const [
+                      Text(
+                        '₹ 2,957',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          CircleAvatar(
+                            radius: 13,
+                            backgroundColor: Color.fromARGB(255, 85, 145, 141),
+                            child: Icon(
+                              Icons.arrow_downward,
+                              color: Colors.white,
+                              size: 19,
+                            ),
+                          ),
+                          SizedBox(width: 7),
+                          Text(
+                            'Income',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 216, 216, 216)),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          CircleAvatar(
+                            radius: 13,
+                            backgroundColor: Color.fromARGB(255, 85, 145, 141),
+                            child: Icon(
+                              Icons.arrow_upward,
+                              color: Colors.white,
+                              size: 19,
+                            ),
+                          ),
+                          SizedBox(width: 7),
+                          Text(
+                            'Expenses',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 216, 216, 216)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        '₹ 1,450',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        '₹ 450',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                )
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
